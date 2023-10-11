a589623f53f0fb75bc0370bfeea20a580b040472
Geoff <gberl001@monroecc.edu>
2023-10-11 17:36:30 -0400

    Fixing issues #14 and #17 discovered in previous semester

--- Week 6 - User Functions & Testing/testMonthlyLoanPayment.m
+++ Week 6 - User Functions & Testing/testMonthlyLoanPayment.m
@@ -1,8 +1,9 @@
-function tests = testMontlyLoanPayment
+function tests = testMonthlyLoanPayment
 clc;
 tests = functiontests(localfunctions);
 end
 
+% Test a few various valid cases
 function testValidSolutionArray(testCase)
 principals = [10000, 5500.25, 22999.99];
 rates = [0.03, 0.05, 0.01];
@@ -15,6 +16,7 @@ actualPmts = getMonthlyLoanPayment(principals, rates, pmtCounts);
 verifyEqual(testCase, actualPmts, expectedPmts, 'AbsTol', 0.001);
 end
 
+% Test a valid solution with a scalar principal
 function testValidSolutionScalar(testCase)
 principal = 40000;    % $40,000
 rate = 0.03;          % 3% interest rate
@@ -32,6 +34,7 @@ verifyTrue(testCase, abs(actualPmt-expectedPmt) <= 0.001);
 
 end
 
+% Test a zero principal amount returns 0 as payment
 function testZeroPrincipal(testCase)
 principal = 0;        % $0
 rate = 0.03;          % 3% interest rate
@@ -44,18 +47,20 @@ actualPmt = getMonthlyLoanPayment(principal, rate, numPmts);
 verifyEqual(testCase, actualPmt, expectedPmt);
 end
 
+% Test that negative principal returns blank indicating an error
 function testNegativePrincipal(testCase)
 principal = -10000;
 rate = 0.03;
 numPmts = 5*12;
 
 % Get expected and actual
-expectedPmt = 0;
+expectedPmt = [];
 actualPmt = getMonthlyLoanPayment(principal, rate, numPmts);
 
 verifyEqual(testCase, actualPmt, expectedPmt);
 end
 
+% Test that a negative rate returns blank indicating an error
 function testNegativeRate(testCase)
 principal = 10000;
 rate = -0.03;
@@ -68,6 +73,7 @@ actualPmt = getMonthlyLoanPayment(principal, rate, numPmts);
 verifyEqual(testCase, actualPmt, expectedPmt);
 end
 
+% Test an empty principal value returns blank indicating an error
 function testEmptyPrincipal(testCase)
 expectedPmt = [];
 actualPmt = getMonthlyLoanPayment([], 0.04, 10);
@@ -76,6 +82,7 @@ verifyEqual(testCase, actualPmt, expectedPmt);
 
 end
 
+% Test an empty rate value returns a blank indicating an error
 function testEmptyRate(testCase)
 expectedPmt = [];
 actualPmt = getMonthlyLoanPayment(10000, [], 10);
@@ -84,6 +91,7 @@ verifyEqual(testCase, actualPmt, expectedPmt);
 
 end
 
+% Test an empty number of payments value returns blank indicating an error
 function testEmptyNumPayments(testCase)
 expectedPmt = [];
 actualPmt = getMonthlyLoanPayment(1000, 0.04, []);
@@ -92,6 +100,7 @@ verifyEqual(testCase, actualPmt, expectedPmt);
 
 end
 
+% Test a string value for principal returns blank indicating an error
 function testPrincipalString(testCase)
 expectedPmt=[];
 actualPmt=getMonthlyLoanPayment("test", 0.04, 10);
@@ -99,6 +108,7 @@ actualPmt=getMonthlyLoanPayment("test", 0.04, 10);
 verifyEqual(testCase, actualPmt, expectedPmt);
 end
 
+% Test a string value for rate returns blank indicating an error
 function testRateString(testCase)
 expectedPmt=[];
 actualPmt=getMonthlyLoanPayment(10000, "rate", 10);
@@ -106,6 +116,7 @@ actualPmt=getMonthlyLoanPayment(10000, "rate", 10);
 verifyEqual(testCase, actualPmt, expectedPmt);
 end
 
+% Test a string number of payments value returns blank indicating an error
 function testNumPaymentsString(testCase)
 expectedPmt=[];
 actualPmt=getMonthlyLoanPayment(10000, 0.04, "ten");
