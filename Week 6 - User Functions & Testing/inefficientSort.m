06190a73fe8c6289341b467e83999b851df5e8b9
Geoff <gberl001@monroecc.edu>
2023-10-11 18:23:11 -0400

    Adding documentation back to inefficientSort

--- Week 6 - User Functions & Testing/inefficientSort.m
+++ Week 6 - User Functions & Testing/inefficientSort.m
@@ -1,6 +1,26 @@
 function result = inefficientSort(x)
-lenX = length(x);
-result = zeros(1,lenX);
+% INEFFICIENTSORT  sort a vector inefficiently
+% 
+%   This is an example of a very inefficient sorting algorithm that 
+%   only supports sorting numeric values in descending order.
+%   
+%   This function is only intended to show how you can easily break 
+%   up your code into smaller, more manageable tasks by using 
+%   subfunctions. Doing so makes your code a little easier to read 
+%   and a little easier to manage when making changes. For example, 
+%   this algorithm would have normally been a nested for-loop, and 
+%   while it still technically is, it makes the code much easier to 
+%   follow by putting the nested for loop in its own function so the 
+%   code is much cleaner.
+% 
+%   Example:
+%     From the Command Window, run the following lines of code
+%     x = [1:10, -20:3:-5, 12:2:36];
+%     sortedX = inefficientSort(x);
+%     lenX = length(x);
+%     result = zeros(1,lenX);
+% 
+%   See also SORT
 
 for n=1:lenX
     % Get the position of the maximum value
